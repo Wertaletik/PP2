48d42071aa9ef4e7f581d624bb0f1ea931c17aef
Wertaletik <karimovsultan1104@gmail.com>
2026-01-20 14:23:40 +0500

    expanded syntax.py

---
+++ practice1/Python-basics/syntax.py
@@ -0,0 +1,15 @@
+print("Hello, World!")
+
+if 5>2:
+    print("YES")
+
+print('Python is "fun"!')
+
+print('Hello,'); print('World!')
+
+print('Hello, World!')
+print("Hello, World!")
+
+print("Hello World!")
+print("I am learning Python.")
+print("It is awesome!")
