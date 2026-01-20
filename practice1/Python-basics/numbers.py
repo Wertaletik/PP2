9525099fa3ac8835f0a7115de187e7dce5fc4d14
Wertaletik <karimovsultan1104@gmail.com>
2026-01-20 14:38:21 +0500

    w3schools numbers

---
+++ practice1/Python-basics/numbers.py
@@ -0,0 +1,52 @@
+x = 1 #int
+y = 2.8 #float
+z = 1j #complex
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+x = 1 #int
+y = 35656222554887711 #int
+z = -3255522 #int
+print(type(x))
+print(type(y))
+print(type(z)) 
+
+
+x = 1.10 #float
+y = 1.0 #float
+z = -35.59 #float
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+x = 35e3 #float
+y = 12E4 #float
+z = -87.7e100 #float
+print(type(x))
+print(type(y))
+print(type(z)) 
+
+
+x = 3+5j #complex
+y = 5j #complex
+z = -5j #complex
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+x = 1 #int
+y = 2.8 #float
+z = 1j #complex
+a = float(x)
+b = int(y)
+c = complex(x)
+print(a)
+print(b)
+print(c)
+print(type(a))
+print(type(b))
+print(type(c))
