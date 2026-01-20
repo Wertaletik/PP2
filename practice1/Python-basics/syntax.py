eb23f171cc8f8da920916bc3adbe239b4d9fc42a
Wertaletik <karimovsultan1104@gmail.com>
2026-01-20 14:26:03 +0500

    w3schools output in syntax

--- practice1/Python-basics/syntax.py
+++ practice1/Python-basics/syntax.py
@@ -13,3 +13,12 @@ print("Hello, World!")
 print("Hello World!")
 print("I am learning Python.")
 print("It is awesome!")
+
+print(3)
+print(358)
+print(50000)
+
+print(3 + 3)
+print(2 * 5)
+
+print("I am", 35, "years old.")
