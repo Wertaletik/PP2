4ce8ae966fb337cf3bddea8ece2406021b96653e
Wertaletik <karimovsultan1104@gmail.com>
2026-02-03 08:34:50 +0500

    Practice 2 - boolean_operator code added

---
+++ practice2/boolean/boolean_operators.py
@@ -0,0 +1,19 @@
+x=3
+y=5
+
+
+#AND
+print(x!=y and x>y) #True and False = False
+print(x!=y and x<y) #True and True = True
+print(x==y and x>=y) #False and False = False
+
+
+#OR
+print(x!=y or x>y) #True or False = True
+print(x!=y or x<y) #True or True = True
+print(x==y or x>=y) #False or False = False
+
+
+#NOT
+print(not x<=y) #not True = False
+print(not x==y) #not False = True
