4bf814f537b02971f0de656e8a96847dabc739ed
Wertaletik <karimovsultan1104@gmail.com>
2026-02-10 09:35:15 +0500

    Practice 3 - added multiple_inheritance

---
+++ practice3/inheritance/multiple_inheritance.py
@@ -0,0 +1,72 @@
+class A:
+    a = 1
+
+class B:
+    b = 2
+
+class R(A, B):
+    pass
+
+r = R()
+print(r.a, r.b)
+
+
+
+class First:
+    def __init__(self):
+        print("First")
+
+class Second:
+    def __init__(self):
+        print("Second")
+
+class Both(First, Second):
+    def __init__(self):
+        First.__init__(self)
+        Second.__init__(self)
+        print("Both")
+
+Both()
+
+
+
+class Alpha:
+    def hi(self):
+        print("Hi Alpha")
+
+class Beta:
+    def hi(self):
+        print("Hi Beta")
+
+class Gamma(Alpha, Beta):
+    def hi(self):
+        super().hi()
+        print("Hi Gamma")
+
+Gamma().hi()
+
+class Gamma(Beta, Alpha):
+    def hi(self):
+        super().hi()
+        print("Hi Gamma")
+
+Gamma().hi()
+
+
+
+class Top:
+    def show(self):
+        print("Top")
+
+class Left(Top):
+    def show(self):
+        print("Left")
+
+class Right(Top):
+    def show(self):
+        print("Right")
+
+class Bottom(Left, Right):
+    pass
+
+Bottom().show()
