cce9289284be4f4a4aed49920b1608482265a7a8
Wertaletik <karimovsultan1104@gmail.com>
2026-02-10 09:27:34 +0500

    Practice 3 - added method_overriding

---
+++ practice3/inheritance/method_overriding.py
@@ -0,0 +1,64 @@
+class Beast:
+    def sound(self):
+        print("Noise")
+
+class Kitty(Beast):
+    def sound(self):
+        print("Meow")
+
+Kitty().sound()
+
+
+
+class Form:
+    def area(self):
+        return 0
+
+class Quad(Form):
+    def __init__(self, a):
+        self.a = a
+
+    def area(self):
+        return self.a ** 2
+
+print(Quad(2).area())
+
+
+
+class Human:
+    def hi(self):
+        print("Hi")
+
+class Pupil(Human):
+    def hi(self):
+        print("Hi, pupil")
+
+Pupil().hi()
+
+
+
+class Ride:
+    def speed(self):
+        return '30 km per hour'
+
+class Auto(Ride):
+    def speed(self):
+        return '60 km per hour'
+
+print(Auto().speed())
+
+
+
+class Wallet:
+    def take(self, x):
+        print(f"Take {x}")
+
+class Safe(Wallet):
+    def take(self, x):
+        if x > 5:
+            print("Too much")
+        else:
+            super().take(x)
+
+Safe().take(3)
+Safe().take(7)
