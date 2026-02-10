cc7748d75b54896a77b8fdafce0d117fa8ab06ab
Wertaletik <karimovsultan1104@gmail.com>
2026-02-10 07:46:20 +0500

    Practice 3 - added basic_functions

---
+++ practice3/functions/basic_functions.py
@@ -0,0 +1,16 @@
+def my_function():
+  print("Hello from a function")
+my_function()
+
+def greet(name):
+    return f"Hello, {name}!"
+print(greet("Teto"))
+
+def add(a, b):
+    return a + b
+
+def power(base, exponent=2):
+    return base ** exponent
+
+def introduce(name, age, country="Iraq"):
+    return f"My name is {name}, I am {age} years old, from {country}."
