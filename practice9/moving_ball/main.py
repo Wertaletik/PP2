0e40c6516ae88ea7d75dfb2aa6c05dd0a68d4758
Wertaletik <karimovsultan1104@gmail.com>
2026-04-14 13:19:50 +0500

    Practice 9 - format editing

--- practice9/moving_ball/main.py
+++ practice9/moving_ball/main.py
@@ -3,11 +3,11 @@ from ball import Ball
 
 pygame.init()
 
-WIDTH, HEIGHT = 650, 450
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+width, height = 650, 450
+screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 
-ball = Ball(WIDTH // 2, HEIGHT // 2)
+ball = Ball(width // 2, height // 2)
 
 running = True
 while running:
@@ -19,13 +19,13 @@ while running:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                ball.move(-ball.step, 0, WIDTH, HEIGHT)
+                ball.move(-ball.step, 0, width, height)
             elif event.key == pygame.K_RIGHT:
-                ball.move(ball.step, 0, WIDTH, HEIGHT)
+                ball.move(ball.step, 0, width, height)
             elif event.key == pygame.K_UP:
-                ball.move(0, -ball.step, WIDTH, HEIGHT)
+                ball.move(0, -ball.step, width, height)
             elif event.key == pygame.K_DOWN:
-                ball.move(0, ball.step, WIDTH, HEIGHT)
+                ball.move(0, ball.step, width, height)
 
     ball.draw(screen)
 
